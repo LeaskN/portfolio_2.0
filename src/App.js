39ea57df819f51b644ab2cc5689b0179138858cc
Nicholas Leask <leask.nicholas@gmail.com>
2021-06-24 18:17:00 -0400

    feat: created NavBar basic files & imported into app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <h1>Test</h1>
     </div>
   );
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css';
+
+function NavBar(){
+    return(
+        <div className='navBar'>
+            <h1>Logo</h1>
+            <h1>Hello</h1>
+            <h1>Landing</h1>
+            <h1>About</h1>
+            <h1>Projects</h1>
+            <h1>Contact</h1>
+            <h1>Resume</h1>
+            <h1>light and dark</h1>
+        </div>
+    )
+}
+
+export default NavBar;
